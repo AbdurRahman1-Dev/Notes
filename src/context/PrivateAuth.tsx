7b684199947a39fb1b2cd788b88876e2d41feac9
--add <rahmanaky521@gmail.com>
2024-03-28 21:38:56 +0600

    routing fix

--- src/context/PrivateAuth.tsx
+++ src/context/PrivateAuth.tsx
@@ -1,22 +1,23 @@
 import React, { useContext } from "react";
 import { AuthContext } from "./AuthContext";
-import { useNavigate } from "@tanstack/react-router";
+// import { useNavigate } from "@tanstack/react-router";
 import { Spinner } from "@nextui-org/react";
 
 // interface AuthProviderProps {
 //   children: React.ReactNode;
 // }
 const PrivateAuth: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const { user, isLoading } = useContext(AuthContext);
 
   if (isLoading) {
     return <Spinner color="primary"></Spinner>;
   } else if (!user) {
-    navigate({
-      to: `/signin`,
-    });
+    // navigate({
+    //   to: `/signin`,
+    // });
+    window.location.href = "/signin";
   }
   return children;
 };
